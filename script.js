1cf2b959cc696a6f5f1d675a564e3f43b5feed0d
Rachel <rachel.rolon@gmail.com>
2020-09-07 20:32:54 -0500

    correct validation complete

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function(){
-   let form = document.getElementById('launchForm');
+   console.log("Page Loaded");
    let submit= document.getElementById('formSubmit');
-   submit.addEventListener('click', function(){
-      let pilot = document.getElementById("pilotName");
-      let copilot = document.querySelector("input[name='copilotName']");
-      let fuel = document.querySelector("input[name='fuelLevel']");
-      let cargo = document.querySelector("input[name='cargoMass']");
-     
+   
+   let pilot = document.getElementById("pilotName");
+   let copilot = document.querySelector("input[name='copilotName']");
+   let fuel = document.querySelector("input[name='fuelLevel']");
+   let cargo = document.querySelector("input[name='cargoMass']");
+   
+   let pilotStatus = document.getElementById('pilotStatus')
+   let copilotStatus = document.getElementById('copilotStatus')
+   let fuelStatus = document.getElementById('fuelStatus')
+   let cargoStatus = document.getElementById('cargoStatus')
+
+   submit.addEventListener('click', function(event){
+      console.log("button clicked");
+      console.log('name value--' + pilot.value +'--')
+   
       let alert = "";
       if (pilot.value === '' || copilot.value === '' || fuel.value === '' || cargo.value === '' ) {
          alert += "\nAll feilds are requierd."
-      }
-      if (typeof pilot.value !== "string" || typeof copilot.value !==  "string") {
-         console.log(pilot.value)
-         console.log(typeof pilot.value)
-         console.log(copilot.value)
-         console.log(typeof copilot.value)
+      } 
+      if ( !isNaN(Number(pilot.value)) ||  !isNaN(Number(copilot.value)) ) {
          alert += "\nPilot and Co-Pilot Names cannot be numbers"
       } 
       if (  isNaN(Number(fuel.value)) ||  isNaN(Number(cargo.value)) ) {
@@ -25,15 +30,16 @@ window.addEventListener("load", function(){
       if (alert !== "") {
          window.alert(alert)
          event.preventDefault();
-      }
-      else {
-      let pilotStatus = document.getElementById('pilotStatus')
-      let copilotStatus = document.getElementById('copilotStatus')
-      pilotStatus.innerHTML += `: ${pilot}`
-      copilotStatus.innerHTML += `: ${copilot}`
-      }
-   });
+      }  
+   //    else {
+   //    let faultyItems = document.getElementById("faultyItems")
+   //    let faultyItemsVisible = faultyItems.style.visibility;
+   //    faultyItemsVisible = 'visible';
+   //    pilotStatus.innerHTML = `Pilot ${pilot.value} is ready.`;
+   //    copilotStatus.innerHTML = `Pilot  ${copilot.value} is ready.`;
+   //   }
 
+   });
 
 });
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
