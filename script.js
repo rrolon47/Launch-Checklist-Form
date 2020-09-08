136718af46434d618d48f19c6932e8c8a7cda289
Rachel <rachel.rolon@gmail.com>
2020-09-07 21:38:53 -0500

    update shuttle requirements:  works but page reloads after leaving the click event handler function

--- script.js
+++ script.js
@@ -12,35 +12,37 @@ window.addEventListener("load", function(){
    let copilotStatus = document.getElementById('copilotStatus')
    let fuelStatus = document.getElementById('fuelStatus')
    let cargoStatus = document.getElementById('cargoStatus')
+   let launchStatus = document.getElementById("launchStatus")
+   let faultyItems = document.getElementById("faultyItems");
 
    submit.addEventListener('click', function(event){
       console.log("button clicked");
-      console.log('name value--' + pilot.value +'--')
+      console.log('name value--' + pilot.value +'--');
    
       let alert = "";
       if (pilot.value === '' || copilot.value === '' || fuel.value === '' || cargo.value === '' ) {
-         alert += "\nAll feilds are requierd."
+         alert += "\nAll feilds are requierd.";
       } 
       if ( !isNaN(Number(pilot.value)) ||  !isNaN(Number(copilot.value)) ) {
-         alert += "\nPilot and Co-Pilot Names cannot be numbers"
+         alert += "\nPilot and Co-Pilot Names cannot be numbers";
       } 
       if (  isNaN(Number(fuel.value)) ||  isNaN(Number(cargo.value)) ) {
-         alert += "\nFuel level and Cargo Mass must be numerical values"
+         alert += "\nFuel level and Cargo Mass must be numerical values";
       } 
       if (alert !== "") {
-         window.alert(alert)
+         window.alert(alert);
          event.preventDefault();
       }  
-   //    else {
-   //    let faultyItems = document.getElementById("faultyItems")
-   //    let faultyItemsVisible = faultyItems.style.visibility;
-   //    faultyItemsVisible = 'visible';
-   //    pilotStatus.innerHTML = `Pilot ${pilot.value} is ready.`;
-   //    copilotStatus.innerHTML = `Pilot  ${copilot.value} is ready.`;
-   //   }
-
+      else {
+      pilotStatus.innerHTML = `Pilot ${pilot.value} is ready.`;
+      copilotStatus.innerHTML = `Co-pilot  ${copilot.value} is ready.`;
+      fuel.value < 10000 ? fuelStatus.innerHTML = "Not enough fuel for the journey" : fuelStatus.innerHTML;
+      cargo.value > 10000 ? cargoStatus.innerHTML = "Too much mass for the shuttle to take off" : cargoStatus.innerHTML;
+      fuel.value < 10000 || cargo.value > 10000 ? launchStatus.innerHTML = "Shuttle not ready for launch" : launchStatus.innerHTML = "Shuttle is ready for launch";
+      fuel.value < 10000 || cargo.value > 10000 ? launchStatus.style.color = "red" : launchStatus.style.color = "green";
+      fuel.value < 10000 || cargo.value > 10000 ? faultyItems.style.visibility = 'visible' : faultyItems.style.visibility;
+      }
    });
-
 });
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
